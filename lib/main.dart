7ae2e312c104284e872b4d8f990e53691a2e6acd
Zach Phelps <zachstarphelps@gmail.com>
2022-02-28 14:05:06 -0500

    firebase and favicon

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:alumni_database/profiles/profile_model.dart';
 import 'package:alumni_database/sample_data.dart';
 import 'package:alumni_database/views/profile/profile.dart';
 import 'package:alumni_database/views/search/search.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp();
+
   runApp(const ProviderScope(
       child: MyApp(),
     )
